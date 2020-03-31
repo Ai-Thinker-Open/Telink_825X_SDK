20fc44e10ddb5624837ecc7ddb873d987f5dffb6
xuhongv <xuhongv@yeah.net>
2020-03-31 19:54:20 +0800

    add explain
    add explain

--- example/at/app_config.h
+++ example/at/app_config.h
@@ -24,92 +24,92 @@
 
 /* Enable C linkage for C++ Compilers: */
 #if defined(__cplusplus)
-extern "C" {
+extern "C"
+{
 #endif
 
-#define AT_VERSION "V0.41"
+#define _MODULE_TB_02_DEV_BOARD_
 
-#if defined  _MODULE_TB_01_  //TB01模块
-	#define CONTROL_GPIO GPIO_PC5
-	#define UART_RX_PIN  UART_RX_PB0
+#if defined _MODULE_TB_01_ //TB01模块
+#define CONTROL_GPIO GPIO_PC5
+#define UART_RX_PIN UART_RX_PB0
 #elif defined _MODULE_TB_02_ //TB02模块
-	#define CONTROL_GPIO GPIO_PB7
-	#define UART_RX_PIN  UART_RX_PA0
+#define CONTROL_GPIO GPIO_PB7
+#define UART_RX_PIN UART_RX_PA0
 #elif defined _MODULE_TB_02_DEV_BOARD_ //TB02开发板
-	#define CONTROL_GPIO GPIO_PA0
-	#define UART_RX_PIN  UART_RX_PB7
+#define CONTROL_GPIO GPIO_PA0
+#define UART_RX_PIN UART_RX_PB7
 #else
-	#error "please set module type"
+#error "please set module type"
 #endif
 
 /////////////////// FEATURE SELECT /////////////////////////////////
-#define BLE_APP_PM_ENABLE					0
-#define PM_DEEPSLEEP_RETENTION_ENABLE		1
-#define TEST_CONN_CURRENT_ENABLE            0 //test curr, disable keyscan concerned
-#define BLE_REMOTE_SECURITY_ENABLE      	0
+#define BLE_APP_PM_ENABLE 0
+#define PM_DEEPSLEEP_RETENTION_ENABLE 1
+#define TEST_CONN_CURRENT_ENABLE 0 //test curr, disable keyscan concerned
+#define BLE_REMOTE_SECURITY_ENABLE 0
 
-#define BLT_SOFTWARE_TIMER_ENABLE			1 //enable soft timer
+#define BLT_SOFTWARE_TIMER_ENABLE 1 //enable soft timer
 
 /////////////////// Clock  /////////////////////////////////
-#define CLOCK_SYS_CLOCK_HZ  	24000000
+#define CLOCK_SYS_CLOCK_HZ 24000000
 
-enum{
-	CLOCK_SYS_CLOCK_1S = CLOCK_SYS_CLOCK_HZ,
-	CLOCK_SYS_CLOCK_1MS = (CLOCK_SYS_CLOCK_1S / 1000),
-	CLOCK_SYS_CLOCK_1US = (CLOCK_SYS_CLOCK_1S / 1000000),
-};
+	enum
+	{
+		CLOCK_SYS_CLOCK_1S = CLOCK_SYS_CLOCK_HZ,
+		CLOCK_SYS_CLOCK_1MS = (CLOCK_SYS_CLOCK_1S / 1000),
+		CLOCK_SYS_CLOCK_1US = (CLOCK_SYS_CLOCK_1S / 1000000),
+	};
 
 /////////////////// watchdog  //////////////////////////////
-#define MODULE_WATCHDOG_ENABLE		0
-#define WATCHDOG_INIT_TIMEOUT		500  //ms
+#define MODULE_WATCHDOG_ENABLE 0
+#define WATCHDOG_INIT_TIMEOUT 500 //ms
 
-extern char at_print_buf[256];
+	extern char at_print_buf[256];
 
 /////////////open SWS digital pullup to prevent MCU err, this is must ////////////
-#define PA7_DATA_OUT			1
+#define PA7_DATA_OUT 1
 
-///////////////////////////////////// ATT  HANDLER define ///////////////////////////////////////
-typedef enum
-{
-	ATT_H_START = 0,
-	//// Gap ////
-	/**********************************************************************************************/
-	GenericAccess_PS_H, 					//UUID: 2800, 	VALUE: uuid 1800
-	GenericAccess_DeviceName_CD_H,			//UUID: 2803, 	VALUE:  			Prop: Read | Notify
-	GenericAccess_DeviceName_DP_H,			//UUID: 2A00,   VALUE: device name
-	GenericAccess_Appearance_CD_H,			//UUID: 2803, 	VALUE:  			Prop: Read
-	GenericAccess_Appearance_DP_H,			//UUID: 2A01,	VALUE: appearance
-	CONN_PARAM_CD_H,						//UUID: 2803, 	VALUE:  			Prop: Read
-	CONN_PARAM_DP_H,						//UUID: 2A04,   VALUE: connParameter
+	///////////////////////////////////// ATT  HANDLER define ///////////////////////////////////////
+	typedef enum
+	{
+		ATT_H_START = 0,
+		//// Gap ////
+		/**********************************************************************************************/
+		GenericAccess_PS_H,			   //UUID: 2800, 	VALUE: uuid 1800
+		GenericAccess_DeviceName_CD_H, //UUID: 2803, 	VALUE:  			Prop: Read | Notify
+		GenericAccess_DeviceName_DP_H, //UUID: 2A00,   VALUE: device name
+		GenericAccess_Appearance_CD_H, //UUID: 2803, 	VALUE:  			Prop: Read
+		GenericAccess_Appearance_DP_H, //UUID: 2A01,	VALUE: appearance
+		CONN_PARAM_CD_H,			   //UUID: 2803, 	VALUE:  			Prop: Read
+		CONN_PARAM_DP_H,			   //UUID: 2A04,   VALUE: connParameter
 
+		//// gatt ////
+		/**********************************************************************************************/
+		GenericAttribute_PS_H,				   //UUID: 2800, 	VALUE: uuid 1801
+		GenericAttribute_ServiceChanged_CD_H,  //UUID: 2803, 	VALUE:  			Prop: Indicate
+		GenericAttribute_ServiceChanged_DP_H,  //UUID:	2A05,	VALUE: service change
+		GenericAttribute_ServiceChanged_CCB_H, //UUID: 2902,	VALUE: serviceChangeCCC
 
-	//// gatt ////
-	/**********************************************************************************************/
-	GenericAttribute_PS_H,					//UUID: 2800, 	VALUE: uuid 1801
-	GenericAttribute_ServiceChanged_CD_H,	//UUID: 2803, 	VALUE:  			Prop: Indicate
-	GenericAttribute_ServiceChanged_DP_H,   //UUID:	2A05,	VALUE: service change
-	GenericAttribute_ServiceChanged_CCB_H,	//UUID: 2902,	VALUE: serviceChangeCCC
+		//// device information ////
+		/**********************************************************************************************/
+		DeviceInformation_PS_H,		  //UUID: 2800, 	VALUE: uuid 180A
+		DeviceInformation_pnpID_CD_H, //UUID: 2803, 	VALUE:  			Prop: Read
+		DeviceInformation_pnpID_DP_H, //UUID: 2A50,	VALUE: PnPtrs
 
+		//// SPP ////
+		/**********************************************************************************************/
+		SPP_PS_H, //UUID: 2800, 	VALUE: telink spp service uuid
 
-	//// device information ////
-	/**********************************************************************************************/
-	DeviceInformation_PS_H,					 //UUID: 2800, 	VALUE: uuid 180A
-	DeviceInformation_pnpID_CD_H,			 //UUID: 2803, 	VALUE:  			Prop: Read
-	DeviceInformation_pnpID_DP_H,			 //UUID: 2A50,	VALUE: PnPtrs
+		//server to client
+		SPP_SERVER_TO_CLIENT_CD_H,	 //UUID: 2803, 	VALUE:  			Prop: read | Notify
+		SPP_SERVER_TO_CLIENT_DP_H,	 //UUID: telink spp s2c uuid,  VALUE: SppDataServer2ClientData
+		SPP_SERVER_TO_CLIENT_CCB_H,	 //UUID: 2902, 	VALUE: SppDataServer2ClientDataCCC
+		SPP_SERVER_TO_CLIENT_DESC_H, //UUID: 2901, 	VALUE: TelinkSPPS2CDescriptor
 
-	//// SPP ////
-	/**********************************************************************************************/
-	SPP_PS_H, 							 //UUID: 2800, 	VALUE: telink spp service uuid
+		ATT_END_H,
 
-	//server to client
-	SPP_SERVER_TO_CLIENT_CD_H,		     //UUID: 2803, 	VALUE:  			Prop: read | Notify
-	SPP_SERVER_TO_CLIENT_DP_H,			 //UUID: telink spp s2c uuid,  VALUE: SppDataServer2ClientData
-	SPP_SERVER_TO_CLIENT_CCB_H,			 //UUID: 2902, 	VALUE: SppDataServer2ClientDataCCC
-	SPP_SERVER_TO_CLIENT_DESC_H,		 //UUID: 2901, 	VALUE: TelinkSPPS2CDescriptor
-
-	ATT_END_H,
-
-}ATT_HANDLE;
+	} ATT_HANDLE;
 
 #include "vendor/common/default_config.h"
 
